df6e0de5349fbfe4b7959692e165b84a73e88ccf
nanda-oliveira <fernandaoliveiraufpe@gmail.com>
2020-08-29 16:36:25 -0300

    addexercicioobjeto

--- exercicios/exercicios-arrays.js
+++ exercicios/exercicios-arrays.js
@@ -2,37 +2,67 @@
 1. Crie um array contendo 5 itens de tipos variados de dados (número, texto, objeto, boleano) 
 */
 // ?
+const itens = [2, 'reprograma', true, { curso: 'backend', data :'29/08/2020' }]
+console.log(itens)
 
+console.log(itens[3])
 /*
 Crie uma função chamada `adicionaItem`, que recebe um parâmetro e o adiciona ao array criado.
 A função deverá retornar o array atualizado.
 */
 // ?
+function adicionaItem(item){
+    itens.push(item) //metodo push adiciona um item na ultima posição do array
+    return itens // retorna o array atualizado
+}
+console.log(adicionaItem(false))
 
-/*
-Mostre no console o segundo elemento desse array, criado acima, com a
+/* Mostre no console o segundo elemento desse array, criado acima, com a
 frase: "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+const compras = ['alface', 'tomate', 'cebola']
+
+console.log(`O segundo elemento do array é ${compras[1]}`); //retorna tomate
+
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "Este array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log(`Esse array tem ${compras.length} itens`)
+
+compras.unshift('feijão') //adiciona feijão na primeira posição do array
+console.log(compras)
+
+compras.pop() //remove o ultimo item do array
+console.log(compras)
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
+
 console.log('Números pares entre 10 e 20:');
-// ?
+let numero = 10
+while(numero<=20){
+    if(numero % 2 === 0){
+        console.log(numero)
+    }
+    numero++
+}
+
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
-console.log('Números ímpares entre 10 e 20:');
-// ?
+//console.log('Números ímpares entre 10 e 20:');
+let numero2 = 10
+while(numero2<=20){
+    if(numero2 % 2 !== 0){
+        console.log(numero2)
+    }
+    numero2++
+}
 
 /*
 Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
@@ -41,7 +71,17 @@ Só vamos mudar o range:
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
 console.log('Números pares entre 100 e 120:');
-// ?
+
+for(let num=100; num <= 120; num++){
+    if (num % 2 === 0){
+        console.log(num)
+    }
+}
 
 console.log('Números ímpares entre 111 e 125:');
-// ?
+
+for (let num= 111; num<=125; num++){
+    if (num % 2 !==0){
+        console.log(num)
+    }
+}
--- exercicios/exercicios-objeto.js
+++ exercicios/exercicios-objeto.js
@@ -6,17 +6,36 @@ As propriedades e tipos de valores para cada propriedade desse objeto devem ser:
 - `idade` - Number
 - `altura` - Number
 - `peso` - Number
-- `andando` - Boolean - recebe "falso" por padrão
+- `andando` - Boolean - recebe "falso" por padrão - iniciando com"false"
 - `caminhouQuantosMetros` - Number - recebe "zero" por padrão
 */
-
+const pessoa = {  //variavel começa com letra minuscula, funcao letra maiuscula
+    nome: "Fernanda", //primeira propriedade do objeto, sao separadas por virgulas
+    sobrenome: "Oliveira", //string
+    idade: 24, //number
+    altura: 1.50,
+    peso: 55,
+    andando: false,
+    caminhouQuantosMetros: 0
+}
 
 /*
 Adicione um método ao objeto `pessoa` chamado `fazerAniversario`. O método deve
 alterar o valor da propriedade `idade` dessa pessoa, somando `1` a cada vez que
 for chamado.
+MÉTODO É A MESMA COISA DE FUNÇÃO
 */
+console.log(pessoa)
+pessoa.fazerAniversario = function (){ //nessa função nao tem parametros
+    return pessoa.idade =pessoa.idade + 1 //para acessar a propriedade da função
+}
 
+//objeto original
+console.log(pessoa)
+//função fazer aniversario
+console.log(pessoa.fazerAniversario())
+//objeto modificado
+console.log(pessoa)
 
 /*
 Adicione um método ao objeto `pessoa` chamado `andar`, que terá as seguintes
@@ -28,78 +47,122 @@ valor dessa propriedade a quantidade passada por parâmetro;
 - Ele deverá modificar o valor da propriedade `andando` para o valor
 booleano que representa "verdadeiro";
 */
+pessoa.andar = function(metrosCaminhados){
+    //pessoa.caminhouQuantrosMetros= pessoa.caminhouQuantosMetros + metrosCaminhados
+     pessoa.caminhouQuantosMetros += metrosCaminhados;
+     pessoa.andando= true;
+     return 
+    
+};
 
+console.log(pessoa);
+pessoa.andar(5); //chamando a função andar e passando 5
+console.log(pessoa);
 
 /*
 Adicione um método ao objeto `pessoa` chamado `parar`, que irá modificar o valor
 da propriedade `andando` para o valor booleano que representa "falso".
 */
 
+pessoa.parar = function(){
+    pessoa.andando= false
+    return
+} 
 
+pessoa.parar() //chamando a função
+console.log(pessoa) // mostrando o objeto com andando = false
 /*
 Crie um método chamado `nomeCompleto`, que retorne a frase:
 - "Olá! Meu nome é [NOME] [SOBRENOME]!"
 */
+pessoa.nomeCompleto = function(){
+    return `Olá! Meu nome ${pessoa.nome} ${pessoa.sobrenome}`
+}
+console.log(pessoa.nomeCompleto())
 
 
 /*
 Crie um método chamado `mostrarIdade`, que retorne a frase:
 - "Olá, eu tenho [IDADE] anos!"
 */
-
+pessoa.mostrarIdade = function(){
+    return `Olá, eu tenho ${pessoa.idade} anos!`
+}
+console.log (pessoa.mostrarIdade()) // chamando a funçao para mostrar idade
 
 /*
 Crie um método chamado `mostrarPeso`, que retorne a frase:
 - "Eu peso [PESO]Kg."
 */
-
+pessoa.mostrarPeso= function(){
+return `Eu peso ${pessoa.peso} kg`
+}
+console.log(pessoa.mostrarPeso())
 
 /*
 Crie um método chamado `mostrarAltura` que retorne a frase:
 - "Minha altura é [ALTURA]m."
 */
+pessoa.mostrarAltura = function(){
+    return `Minha altura é ${pessoa.altura}m`
 
-
+}
+console.log(pessoa.mostrarAltura()) //chamando a função para mostrar altura
 /*
 Faça a `pessoa` fazer 3 aniversários.
 */
-
+pessoa.fazerAniversario()
+pessoa.fazerAniversario()
+pessoa.fazerAniversario()
 
 /*
 Quantos anos a `pessoa` tem agora? (Use a instrução para responder e
 comentários inline ao lado da instrução para mostrar qual foi a resposta
 retornada)
 */
-
+console.log(pessoa.idade)
 
 /*
 Agora, faça a `pessoa` caminhar alguns metros, invocando o método `andar` 3x,
 com metragens diferentes passadas por parâmetro.
 */
-
+pessoa.andar(5)
+pessoa.andar(8)
+pessoa.andar(3)
 
 /*
 A pessoa ainda está andando? (Use a instrução para responder e comentários
 inline ao lado da instrução para mostrar qual foi a resposta retornada)
 */
 
-
+console.log(pessoa.caminhouQuantosMetros) // mostra quantos metros a pessoa andou
+console.log(pessoa.andando) //mostra true
 /*
 Se a pessoa ainda está andando, faça-a parar.
 */
-
+pessoa.parar() // faz a pessoa parar de andar
 
 /*
 E agora: a pessoa ainda está andando? (Use uma instrução para responder e
 comentários inline ao lado da instrução para mostrar a resposta retornada)
 */
 
+console.log(pessoa.andando) //mostra false
 
-/*
+/* PULAMOS
 Quantos metros a pessoa andou? (Use uma instrução para responder e comentários
 inline ao lado da instrução para mostrar a resposta retornada)
 */
 
+/*
+Crie um método chamado `mostrarDistancia` que retorne a frase:
+- "[NOME] caminhou [caminhouQuantosMetros]m"
+*/
+pessoa.mostrarDistancia = function(){
+    return `${pessoa.nome} caminhou ${pessoa.caminhouQuantosMetros} m`
+}
+console.log(pessoa.mostrarDistancia()) // chama a função que mostra a distancia
+console.log( `caminhou ${pessoa.caminhouQuantosMetros}`) //mostra distancia
 
 /*
 Crie um método para o objeto `pessoa` chamado `apresentacao`. Esse método deve
@@ -115,6 +178,46 @@ deve conter no retorno da frase acima é "metro" no lugar de "metros".
 método), que será concatenada com a frase de retorno, mostrando a resposta
 correta, de acordo com os dados inseridos no objeto.
 */
+pessoa.apresentacao = function(){
 
+    let anos= "anos"
+    let metros= "metros"
+    
+    if(pessoa.idade===1){
+        anos = "ano"
+    }
+    if (pessoa.caminhouQuantosMetros ===1){
+        metros= "metro"
+    }
+
+    return `Olá, eu sou ${pessoa.nome} ${pessoa.sobrenome}, tenho ${pessoa.idade} ${anos}, minha altura é ${pessoa.altura} , meu peso é ${pessoa.peso} e, só hoje eu já caminhei ${pessoa.caminhouQuantosMetros} ${metros}! `
+}
 
 // Agora, apresente-se ;)
+
+console.log(pessoa.apresentacao())
+
+
+//////////////////////////////
+
+//métodos
+//entries
+
+
+/*const pessoa = { 
+    nome: "Fernanda", 
+    sobrenome: "Oliveira", 
+    idade: 24, 
+    altura: 1.50,
+    peso: 55,
+    andando: false,
+    caminhouQuantosMetros: 0
+}*/
+
+console.log(Object.entries(pessoa)) // retorna as propriedades (keys)+ valor (values) em forma da lista (array)
+
+console.log(Object.keys(pessoa)) //retorna as propriedade em formato de lista(array)
+
+console.log(Object.values(pessoa)) //retorna os valores em formato de lista (array) das propriedades enumeraveis (string, number, boolean)
+
+console.log(pessoa.hasOwnProperty('nome')) //retorna true ou false para a propriedade passada por parametro
