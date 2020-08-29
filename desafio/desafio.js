e0ad605c11d82e94ae3bb9e8e060ead68b1ed8ba
nanda-oliveira <fernandaoliveiraufpe@gmail.com>
2020-08-29 17:00:43 -0300

    rascunhodesafio

--- desafio/desafio.js
+++ desafio/desafio.js
@@ -1,1 +1,24 @@
 //Exercicio 1
+
+const ListadeProjetos= []
+
+//Exercicio 2
+
+const projeto1 = {
+    idProjeto: 1, 
+    tituloDoProjeto: "Semana 04",
+    descricaoDoProjeto: 'exercicio da semana',
+    pessoasResponsaveis: [{IdPessoa:123, nome:'Nanda', setor: 'tecnologia'},{IdPessoa:345, nome:'Julia', setor: 'tecnologia'}, {IdPessoa:356, nome:'Karol', setor: 'tecnologia'} ],
+    PessoasColaboradas: [{IdPessoa:345, nome:'Andreza', setor: 'tecnologia'}],
+    dataInicio: '29/08/2020',
+    statusdoProjeto: "em andamento"
+}
+
+function adicionaProjeto(projeto1){
+    return ListadeProjetos.push(projeto1)
+}
+console.log(ListadeProjetos, 'Lista de Projeto Vazia')
+
+adicionaProjeto()
+
+console.log(ListadeProjetos, 'Lista de Projetos com um Projeto')
