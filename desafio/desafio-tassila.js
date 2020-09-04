7b658c77fc1a9e4995be53259ea0291909350873
Tassila Bomfim <eng.tassilabomfim@gmail.com>
2020-09-04 19:18:12 -0300

    atualizando projeto

--- desafio/desafio-tassila.js
+++ desafio/desafio-tassila.js
@@ -55,3 +55,75 @@ function listarProjetos(){
 }
 
 console.log(listarProjetos());
+
+//Exercicio 5
+
+function posicaoDoProjeto(p){
+    if(listaDeProjetos[p] === undefined){
+    return `Projeto não encontrado`
+} else {
+    return `O ${listaDeProjetos[p].tituloDoProjeto} está na posição ${p} e o seu status é ${listaDeProjetos[p].statusDoProjeto}.`;
+}
+}
+
+console.log(posicaoDoProjeto(1)) //retorna projeot 1
+console.log(posicaoDoProjeto(12))
+
+// Exercicio 6
+
+function totalDeProjetos(){
+    let objetos = listaDeProjetos.length
+    let plural = 'projetos cadastrados';
+    let singular = 'projeto cadastrado';
+
+    if (objetos > 1){
+        return `Temos o total de ${objetos} ${plural}. `
+    } else {
+        return `Temos o total de ${objetos} ${singular}`
+    }
+}
+
+console.log(totalDeProjetos())
+
+//Exercicio 7
+
+function buscarProjeto(idProjeto){
+
+    //atribui a variavel projetoEncontrado o retorno do filtro
+    // projeto é cada posição do array que o filtro vai acessar para bucar nossa condição.
+    // return é o resultado da nossa condição
+
+    let projetoEncontrado = listaDeProjetos.filter(function (projeto){
+    return projeto.idProjeto === idProjeto;
+    });
+    
+    if(projetoEncontrado !== undefined) {
+        return projetoEncontrado
+    } else {
+        return `Projeto não encontrado`;
+    }
+
+    return projetoEncontrado
+}
+
+console.log(buscarProjeto(3)) //retorna projeto 3
+console.log(posicaoDoProjeto(99))
+
+
+//Exercicio 8
+//erroor
+function mostrarResponsavel(idProjeto, idPessoa) {
+    let projeto = buscarProjeto(idProjeto);
+    let responsavel = projeto.pessoasResponsaveis.find(function (responsavel){
+        return responsavel.idPessoa === idPessoa
+    });
+
+  if(responsavel !== undefined){
+  return true;
+  } else {
+      return false;
+  }
+
+}
+
+console.log(mostrarResponsavel(2, 455))
