b692042bf5bbb9d8421c99ef6f4a102e55380efe
shaienne <shaiene@gmail.com>
2020-09-05 09:11:18 -0300

    geral

--- exercicios/desafio.js
+++ exercicios/desafio.js
@@ -1,27 +1,143 @@
-//Exercicio 1
+// Exercicio 1
 
-const listaProjetos = []
+const listaDeProjetos = [];
 
-// Exercicio 2
+// Exercício 2
 
 const projeto1 = {
-    idProjeto: 1, 
-    titleProjeto: 'Semana 4',
-    descricaoProjeto: 'exercicio da semana',
-    pessoasResponsaveis: [{idPessoa: 123, nome: 'Shaiene', setor: 'Marketing'}, {idPessoa: 987, nome: 'Raíza', setor: 'Marketing'}],
-    colaboradores: [{idPessoa:345, nome: 'Andreza', setor: 'tecnologia'}],
-    dataInicio: '29/08/20',
-    statusProjeto: 'em andamento'
-} 
+  idProjeto: 1,
+  tituloDoProjeto: 'Semana 04',
+  descricaoDoProjeto: 'exercicio da semana',
+  pessoasResponsaveis: [
+    { idPessoa: 123, nome: 'Yasminn', setor: 'tecnologia' },
+    { idPessoa: 987, nome: 'Larissa', setor: 'tecnologia' },
+  ],
+  pessoasColaboradoras: [
+    { idPessoa: 345, nome: 'Andreza', setor: 'tecnologia' },
+  ],
+  dataInicio: '29/08/2020',
+  statusDoProjeto: 'em andamento',
+};
 
-function adicionaProjeto(projeto){
-    return listaProjetos.push(projeto)
+const projeto2 = {
+  idProjeto: 2,
+  tituloDoProjeto: 'Noticias Boas',
+  descricaoDoProjeto: 'site para mostrar somente noticias positivas',
+  pessoasResponsaveis: [{ idPessoa: 589, nome: 'Laura', setor: 'marketing' }],
+  pessoasColaboradoras: [
+    { idPessoa: 345, nome: 'Felipe', setor: 'marketing' },
+    { idPessoa: 765, nome: 'Fernanda', setor: 'pessoas' },
+    { idPessoa: 132, nome: 'Amaury', setor: 'tecnologia' },
+  ],
+  dataInicio: '23/07/2020',
+  statusDoProjeto: 'encerrado',
+};
+
+const projeto3 = {
+  idProjeto: 3,
+  tituloDoProjeto: 'Atendo em Libras',
+  descricaoDoProjeto:
+    'projeto para conectar profissionais de saúde a pessoas surdas',
+  pessoasResponsaveis: [
+    { idPessoa: 765, nome: 'Fernanda', setor: 'pessoas' },
+    { idPessoa: 132, nome: 'Amaury', setor: 'tecnologia' },
+  ],
+  pessoasColaboradoras: [
+    { idPessoa: 893, nome: 'Fabiola', setor: 'marketing' },
+  ],
+  dataInicio: '15/03/2020',
+  statusDoProjeto: 'em andamento',
+};
+
+// Exercício 3
+
+function adicionaProjeto(projeto) {
+  return listaDeProjetos.push(projeto);
 }
-console.log(listaProjetos, 'Lista de projetos vazia')
 
-adicionaProjeto(projeto1)
+console.log(listaDeProjetos, 'Lista de projetos vazia');
 
-console.log(listaProjetos, 'Lista de projetos com um projeto')
+adicionaProjeto(projeto1); // adiciona o projeto1 a lista de projetos utilizando a função criada
+adicionaProjeto(projeto2); // adiciona o projeto2 a lista de projetos utilizando a função criada
+adicionaProjeto(projeto3); // adiciona o projeto2 a lista de projetos utilizando a função criada
 
-//Exercicio 3
+console.log(listaDeProjetos, 'Lista de projetos com os projetos adicionados');
 
+// Exercicio 4
+
+function listarProjetos() {
+  // return `Lista de projetos: ${JSON.stringify(listaDeProjetos)}`;
+  return listaDeProjetos;
+}
+
+console.log(listarProjetos());
+
+// Exercicio 5
+
+function posicaoDoProjeto(posicao) {
+  if (listaDeProjetos[posicao] === undefined) {
+    return `Projeto não encontrado`;
+  } else {
+    return `O "${listaDeProjetos[posicao].tituloDoProjeto}" está na posição ${posicao} e seu status é "${listaDeProjetos[posicao].statusDoProjeto}"`;
+  }
+}
+
+console.log(posicaoDoProjeto(0)); // retorna projeto1
+console.log(posicaoDoProjeto(2)); // retorna projeto3
+console.log(posicaoDoProjeto(999)); // retorna Projeto não encontrado
+
+// Exercicio 6
+
+function totalDeProjetos() {
+  let objetos = listaDeProjetos.length;
+  let plural = 'projetos cadastrados';
+  let singular = 'projeto cadastrado';
+
+  if (objetos > 1) {
+    return `Temos o total de ${objetos} ${plural}`;
+  } else {
+    return `Temos o total de ${objetos} ${singular}`;
+  }
+}
+
+console.log(totalDeProjetos()); // retorna 3
+
+// Exercicio 7
+
+function buscarProjeto(idProjeto) {
+  // atribui a variavel projetoEncontrado o retorno do filtro
+  // projeto é cada posição do array que o filtro vai acessar para buscar nossa condição
+  // return é o resultado da nossa condição
+
+  // também poderíamos usar o método find
+
+  // let projetoEncontrado = listaDeProjetos.filter(function (projeto) {
+  //   return projeto.idProjeto === idProjeto;
+  // });
+
+  let projetoEncontrado = listaDeProjetos.find(function (projeto) {
+    return projeto.idProjeto === idProjeto;
+  });
+
+  if (projetoEncontrado !== undefined) {
+    return projetoEncontrado;
+  } else {
+    return `Projeto não encontrado`;
+  }
+}
+
+console.log(buscarProjeto(3)); // retorna projeto3
+console.log(buscarProjeto(999)); // retorna a mensagem
+
+// Exercicio 8
+
+function mostrarResponsavel(idProjeto, idPessoa) {
+  let projeto = buscarProjeto(idProjeto);
+  let responsavel = projeto.pessoasResponsaveis.filter(function (responsavel) {
+    return responsavel.idPessoa === idPessoa;
+  });
+
+  return responsavel;
+}
+
+console.log(mostrarResponsavel(2, 589));
